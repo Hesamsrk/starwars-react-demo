38be98f61ef938ff9d13c62ded38da812c48b8f6
hesamsrk <hesamsrk@gmail.com>
2022-06-18 19:10:38 +0430

    deployed and README.md added

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig, loadEnv, UserConfigExport} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({command, mode}) => {
@@ -6,11 +6,16 @@ export default defineConfig(({command, mode}) => {
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env = loadEnv(mode, process.cwd(), 'VITE_')
     console.log({env})
-    return {
-        // vite config
+
+
+    let config: UserConfigExport = {
         define: {
             __APP_ENV__: env.APP_ENV
         },
-        plugins: [react()]
+        plugins: [react()],
+        base: env.VITE_GH_PAGES_BASE_URL
     }
+
+
+    return config
 })
